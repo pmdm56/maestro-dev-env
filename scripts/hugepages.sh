43e5fb8e7d0a904e541e171ef4511c0e3727ef3e
Francisco Pereira <fchamicapereira@gmail.com>
2022-02-03 12:19:27 +0000

    minor changes

--- scripts/hugepages.sh
+++ scripts/hugepages.sh
@@ -11,7 +11,8 @@ fi
 mkdir -p /mnt/hugepages2M
 mount -t hugetlbfs -o pagesize=2M none /mnt/hugepages2M
 
-su -c "echo 256 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages"
+# ~64 MB of hugepages
+su -c "echo 32 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages"
 
 echo "*********** /proc/meminfo content *********** "
 cat /proc/meminfo | grep Huge
