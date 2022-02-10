4eb2d3588d4378063a921042187d01373d3c344b
Francisco Pereira <fchamicapereira@gmail.com>
2022-02-10 19:06:22 +0000

    check if the scripts runs on macOS and alert the user

--- run-vigor.sh
+++ run-vigor.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPT_DIR
--- scripts/hugepages.sh
+++ scripts/hugepages.sh
@@ -1,15 +1,33 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 set -euo pipefail
 
+SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+HUGEPAGES="$SCRIPT_DIR/../hugepages2M"
+
+# Check if we are running on linux
+host_os=$(uname -s)
+
+if [ "$host_os" != "Linux" ]; then
+  echo ""
+  echo "************************** WARNING *************************** "
+  echo "* This script only knows how to allocate hugepages on Linux. * "
+  echo "* If you are currently running Windows or MacOS, try         * "
+  echo "* allocating hugepages inside the container.                 * "
+  echo "************************************************************** "
+  echo ""
+
+  exit 0
+fi
+
 # We need to be root
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
   exit
 fi
 
-mkdir -p /mnt/hugepages2M
-mount -t hugetlbfs -o pagesize=2M none /mnt/hugepages2M
+mkdir -p $HUGEPAGES
+mount -t hugetlbfs -o pagesize=2M none $HUGEPAGES
 
 # ~64 MB of hugepages
 su -c "echo 32 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages"
